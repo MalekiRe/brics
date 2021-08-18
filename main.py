4e28b53163c381a2b63410c12856ab71321e58f2
lglik <leoglikbarg@gmail.com>
2021-08-18 15:43:24 -0700

    made logger global variables instead of args

--- main.py
+++ main.py
@@ -27,18 +27,18 @@ def secure_sleep(sleep_length):
         gap_time = time.time() - start
 
 
-def get_humidity(sensor, data):
+def get_humidity(sensor):
     """get the sensed humidty from the humidity sensor and return the value"""
     hum = sensor.data.humidity
     data.info(("humidity: {humidity} %RH").format(humidity=hum))
 
-def get_temperature(sensor, data):
+def get_temperature(sensor):
     """get the sensed temperature  from the temperature sensor and return the value"""
     temp = sensor.data.temperature
     data.info(("temperature: {temperature} C").format(temperature=temp))
 
 
-def open_valve(servo, status):
+def open_valve(servo):
     """actuate a servo to open a pinch valve"""
     status.info(("attempting to open valve {valve_id}").format(valve_id=servo))
     servos[servo]["instance"].ChangeDutyCycle(9.5) # valve opening
@@ -48,7 +48,7 @@ def open_valve(servo, status):
     status.info(("valve {valve_id} open").format(valve_id=servo))
 
 
-def close_valve(servo, status):
+def close_valve(servo):
     """actuate a servo to close a pinch valve"""
     status.info(("attempting to close valve {valve_id}").format(valve_id=servo))
     servos[servo]["instance"].ChangeDutyCycle(5.0) # valve closing
@@ -98,14 +98,14 @@ def log_sense(wait, status, data):
     while active:
         # if there is new data, get it
         if sensor.get_sensor_data():
-            get_humidity(sensor, data)
-            get_temperature(sensor, data)
+            get_humidity(sensor)
+            get_temperature(sensor)
         status.info(("waiting {wait_time}s before recording more sensor data").format(wait_time=wait))
         secure_sleep(wait)
     status.info("sensor data collection ended")
 
 
-def configure_servos(servo_pair_num, status):
+def configure_servos(servo_pair_num):
     """create a number of paired servo objects with specific adresses and set them to be closed"""
     global servos
     servos = {}
@@ -128,14 +128,16 @@ def configure_servos(servo_pair_num, status):
         servos[servo_name_a] = {"channel": servo_channel_a, "instance": servo_a}
         servos[servo_name_b] = {"channel": servo_channel_b, "instance": servo_b}
         # start servos as closed
-        close_valve(servo_name_a, status)
-        close_valve(servo_name_b, status)
+        close_valve(servo_name_a)
+        close_valve(servo_name_b)
 
 
 def run(sense_report_time, test_start_time, short_gap_time, long_gap_time, test_end_time):
     """periodically actuate servos to open/close valves and log sensor data"""
     # setup loggers and data collection
     global active
+    global status
+    global data
     status = configure_logger("status", "status.log")
     data = configure_logger("data", "data.log")
     status.info("loggers configured")
@@ -143,16 +145,16 @@ def run(sense_report_time, test_start_time, short_gap_time, long_gap_time, test_
         sense_thread  = threading.Thread(target=log_sense, args=(sense_report_time, status, data))
         sense_thread.start()
         servo_pair_num = int((len(servo_ports)/2))
-        configure_servos(servo_pair_num, status)
+        configure_servos(servo_pair_num)
         status.info("servos configured")
         # active testing
         status.info(("waiting {start_wait} minutes before beginning first valve movement").format(start_wait=float(test_start_time)/60.0))
         secure_sleep(test_start_time)
         for pair_num in range(1, servo_pair_num + 1):
-            open_valve(("{num}a").format(num=pair_num), status)
+            open_valve(("{num}a").format(num=pair_num))
             secure_sleep(short_gap_time)
             status.info(("waiting {gap_time}s before moving on").format(gap_time=short_gap_time))
-            open_valve(("{num}b").format(num=pair_num), status)
+            open_valve(("{num}b").format(num=pair_num))
             secure_sleep(long_gap_time)
             status.info(("waiting {gap_time} hours before moving on").format(gap_time=float(short_gap_time)/3600.0))
         # cleanup
@@ -173,6 +175,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 5:
         run(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     else:
-        # dev: 1hz sensor spped 15s, 10s, 20s, 12s
+        # dev: 1hz sensor spped, 15s, 10s, 20s, 12s
         # flight: 1hz sensor speed, 1 hour, 10s, 48 hours, 120 hours
         run(1, 3600, 10, 3600*48, 3600*120)

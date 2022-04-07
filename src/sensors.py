11bf57558c0b1ad0862e143f3717be5eaaf2357c
MalekiRe <pocmalek@gmail.com>
2022-04-07 00:19:09 -0700

    Update sensors.py

--- src/sensors.py
+++ src/sensors.py
@@ -98,7 +98,7 @@ def get_data(sensor, bus):
         print('Temp: {} C'.format(round(sensor.temperature, 3)))
         print('Gas: {} ohms'.format(round(sensor.gas, 3)))
         print('Hum: {}%'.format(round(sensor.humidity, 3)))
-        print('Press: {}hPa'.format(round(sensor.pressure, 3)))
+        print('Press: {} hPa'.format(round(sensor.pressure, 3)))
         print("-"*8)
    
 if __name__ == "__main__":
